9caa7d9f0be6132f75473998205bda152acbb6ab
Werfmon <d.vyroubal.w@gmail.com>
2022-10-04 16:08:16 +0200

    fnc in settings to service

--- src/Views/Settings/Settings.tsx
+++ src/Views/Settings/Settings.tsx
@@ -1,27 +1,24 @@
 import React, {useState} from 'react';
 
 import { navigate } from '../../Components/Navigation/RootNavigation';
+import { logout } from '../../Services/Settings/logout';
 
-import { removeTokenFromStorage } from '../../Helper/removeTokenFromStorage';
 import { SettingsContainer } from './Components/SettingsContainer';
 import { ContainerHeading } from './Components/ContainerHeading';
+import { ButtonContainer } from './Components/ButtonContainer';
 import { SettingsChild } from './Components/SettinsChild';
+import SwitchButton from './Components/SwitchButton';
 import { MainView } from '../_Components/MainView';
 import { Heading } from './Components/Heading';
-import { Title } from './Components/Title';
 import Button from '../_Components/Button';
-import SwitchButton from './Components/SwitchButton';
-import { ButtonContainer } from './Components/ButtonContainer';
+import { Title } from './Components/Title';
+
 
 
 const Settings = () => {
   const [addPeopleState, setAddPeopleState] = useState<boolean>(false);
   const [addPeopleState2, setAddPeopleState2] = useState<boolean>(false);
 
-  function logout(): void {
-    removeTokenFromStorage();
-    navigate('Login')
-  }
   return (
     <MainView>
       <ContainerHeading>
