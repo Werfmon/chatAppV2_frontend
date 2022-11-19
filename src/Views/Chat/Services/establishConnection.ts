a508bfb51b4d0365d35b277fc388d3dee095838b
Werfmon <d.vyroubal.w@gmail.com>
2022-11-20 00:55:41 +0100

    CA-3 handlig socket errors

--- src/Views/Chat/Services/establishConnection.ts
+++ src/Views/Chat/Services/establishConnection.ts
@@ -1,7 +1,10 @@
 import { Dispatch, SetStateAction } from "react";
 import { getTokenFromStorage } from "../../../Helper/getTokenFromStorage";
+import { removeError } from "../../_Components/ErrorHanding/Error";
+import { Status } from "../../_Components/ErrorHanding/Helper/Status";
+import { ErrorProps } from "../../_Components/ErrorHanding/Types/ErrorProps";
 
-export function establishConnection(setWebSocket: Dispatch<SetStateAction<WebSocket>>, WS_URL: string) {
+export function establishConnection(setWebSocket: Dispatch<SetStateAction<WebSocket>>, WS_URL: string, setError: Dispatch<SetStateAction<ErrorProps>>) {
     getTokenFromStorage().then((token) => {
         const headers = { Authorization: "" };
         headers["Authorization"] = `Bearer ${token}`;
@@ -14,7 +17,9 @@ export function establishConnection(setWebSocket: Dispatch<SetStateAction<WebSoc
         };
   
         webSocketLocal.onerror = (e: any) => {
-          console.error(e);
+          setError({message: 'Something went wrong, try logout and login again', status: Status.WARNING, show: true});
+          removeError(setError);
+          console.warn(e);
         };
       });
 }
