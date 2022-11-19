2d1b9ab6dbcc3a736714ff2af956f24ae6b22908
Werfmon <d.vyroubal.w@gmail.com>
2022-11-19 19:22:49 +0100

    Implementation of error messages

---
+++ src/Views/_Components/ErrorHanding/Components/Text.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Text = styled.Text`
+    color: #fff;
+    text-align: center;
+    padding-top: 3px;
+    padding-bottom: 4px;
+    padding-left: 10px;
+    padding-right: 10px;
+`
---
+++ src/Views/_Components/ErrorHanding/Helper/Status.ts
@@ -0,0 +1,5 @@
+export enum Status {
+  INFO = "#0084ff",
+  WARNING = "#ff9100",
+  ERROR = "#dd0707",
+}
